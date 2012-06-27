3a217525c826ec9cc3a9b130c3c661825ab7dad6
Daisuke Miyakawa <dmiyakawa@google.com>
2012-06-27 12:45:36 -0700

    Do not allow intents to initiate special char sequence handling.
    This is a squash of the following jellybean commits backported to ICS:
    
    - Prevent Intent from initiating special-char handling
      Ic5b042f620b10931fedcaf12bb58be2405bf7390
    
    - Remember the flag suppressing special char handling
      Ie62a448675ac558ecdeea43da01082712edee35a
    
    - Follow-up to Ie62a4486
      Idea7ae599e3a217ad656a304fbae26746d9f3284
    
    (Consolidated commit message from maniac103)
    
    Change-Id: If4d9cff84e5202a4a9693e9824acebf1da2afa88

--- src/com/android/contacts/dialpad/DialpadFragment.java
+++ src/com/android/contacts/dialpad/DialpadFragment.java
@@ -211,6 +211,16 @@ public class DialpadFragment extends Fragment
 
     private boolean mWasEmptyBeforeTextChange;
 
+    /**
+     * This field is set to true while processing an incoming DIAL intent, in order to make sure
+     * that SpecialCharSequenceMgr actions can be triggered by user input but *not* by a
+     * tel: URI passed by some other app.  It will be set to false when all digits are cleared.
+     */
+    private boolean mDigitsFilledByIntent;
+
+    private static final String PREF_DIGITS_FILLED_BY_INTENT = "pref_digits_filled_by_intent";
+
+    @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         mWasEmptyBeforeTextChange = TextUtils.isEmpty(s);
     }
@@ -228,12 +238,14 @@ public class DialpadFragment extends Fragment
     }
 
     public void afterTextChanged(Editable input) {
-        if (SpecialCharSequenceMgr.handleChars(getActivity(), input.toString(), mDigits)) {
+        if (!mDigitsFilledByIntent &&
+                SpecialCharSequenceMgr.handleChars(getActivity(), input.toString(), mDigits)) {
             // A special sequence was entered, clear the digits
             mDigits.getText().clear();
         }
 
         if (isDigitsEmpty()) {
+            mDigitsFilledByIntent = false;
             mDigits.setCursorVisible(false);
         }
 
@@ -258,6 +270,10 @@ public class DialpadFragment extends Fragment
 
         mProhibitedPhoneNumberRegexp = getResources().getString(
                 R.string.config_prohibited_phone_number_regexp);
+
+        if (state != null) {
+            mDigitsFilledByIntent = state.getBoolean(PREF_DIGITS_FILLED_BY_INTENT);
+        }
     }
 
     @Override
@@ -382,6 +398,8 @@ public class DialpadFragment extends Fragment
                 if ("tel".equals(uri.getScheme())) {
                     // Put the requested number into the input area
                     String data = uri.getSchemeSpecificPart();
+                    // Remember it is filled via Intent.
+                    mDigitsFilledByIntent = true;
                     setFormattedDigits(data, null);
                     return true;
                 } else {
@@ -395,6 +413,8 @@ public class DialpadFragment extends Fragment
                         if (c != null) {
                             try {
                                 if (c.moveToFirst()) {
+                                    // Remember it is filled via Intent.
+                                    mDigitsFilledByIntent = true;
                                     // Put the number into the input area
                                     setFormattedDigits(c.getString(0), c.getString(1));
                                     return true;
@@ -488,6 +508,9 @@ public class DialpadFragment extends Fragment
         showDialpadChooser(needToShowDialpadChooser);
     }
 
+    /**
+     * Sets formatted digits to digits field.
+     */
     private void setFormattedDigits(String data, String normalizedNumber) {
         // strip the non-dialable numbers out of the data string.
         String dialString = PhoneNumberUtils.extractNetworkPortion(data);
@@ -622,6 +645,12 @@ public class DialpadFragment extends Fragment
         // TODO: I wonder if we should not check if the AsyncTask that
         // lookup the last dialed number has completed.
         mLastNumberDialed = EMPTY_NUMBER;  // Since we are going to query again, free stale number.
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(PREF_DIGITS_FILLED_BY_INTENT, mDigitsFilledByIntent);
     }
 
     @Override
